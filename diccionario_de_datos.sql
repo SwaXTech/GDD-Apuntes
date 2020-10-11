b453dc168ae95d60998d195536cca48087cbf8eb
SwaXTech <aleeherasimiuk@gmail.com>
2020-10-11 19:27:42 -0300

    Diccionario de datos

---
+++ diccionario_de_datos.sql
@@ -0,0 +1,3 @@
+SELECT * FROM SYS.TRIGGERS -- Datos particulares de cada trigger
+SELECT * FROM SYS.OBJECTS WHERE type = 'TR' -- Data de todos los objetos
+SELECT * FROM SYS.ALL_SQL_MODULES WHERE object_id = 1637580872 -- Contiene la definicion de cada objeto, Util para cuando se quiere recuperar lo borrado
